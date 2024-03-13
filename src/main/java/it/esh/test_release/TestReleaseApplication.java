246350f3f62e945ecebf54fe8530ce04aa4f09a9
simone.spinelli <s.spinelli@elitesoftwarehouse.com>
2024-03-13 07:26:31 +0100

    first commit

---
+++ src/main/java/it/esh/test_release/TestReleaseApplication.java
@@ -0,0 +1,13 @@
+package it.esh.test_release;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestReleaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestReleaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/esh/test_release/TestReleaseApplicationTests.java
@@ -0,0 +1,13 @@
+package it.esh.test_release;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestReleaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
